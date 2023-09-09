d6fec79c6902f7b78fcf333423c776f10b3f3806
bruh <quangtrung02hn16@gmail.com>
2023-09-09 13:22:14 +0700

    Add bottom sheet stuff

--- stuff/src/main/java/com/sixtyninefourtwenty/stuff/BottomSheetDialogs.kt
+++ stuff/src/main/java/com/sixtyninefourtwenty/stuff/BottomSheetDialogs.kt
@@ -3,6 +3,7 @@
 
 package com.sixtyninefourtwenty.stuff
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -10,3 +11,19 @@ val BottomSheetDialogFragment.bottomSheetDialog: BottomSheetDialog?
     get() = dialog as BottomSheetDialog?
 
 fun BottomSheetDialogFragment.requireBottomSheetDialog() = requireDialog() as BottomSheetDialog
+
+fun BottomSheetDialog.expand() {
+    behavior.state = BottomSheetBehavior.STATE_EXPANDED
+}
+
+fun BottomSheetDialog.collapse() {
+    behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+}
+
+fun BottomSheetDialog.hides() {
+    behavior.state = BottomSheetBehavior.STATE_HIDDEN
+}
+
+fun BottomSheetDialog.halfExpand() {
+    behavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+}
