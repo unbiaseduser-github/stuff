66b6e40266bcf26401d7d21cd6c61345f7d952d6
bruh <quangtrung02hn16@gmail.com>
2023-09-09 13:21:57 +0700

    Add locale stuff

--- stuff/src/main/java/com/sixtyninefourtwenty/stuff/Locales.kt
+++ stuff/src/main/java/com/sixtyninefourtwenty/stuff/Locales.kt
@@ -6,4 +6,14 @@ package com.sixtyninefourtwenty.stuff
 import androidx.appcompat.app.AppCompatDelegate
 import java.util.Locale
 
-fun obtainAppLocale(): Locale = AppCompatDelegate.getApplicationLocales()[0] ?: Locale.getDefault()
+/**
+ * Similar to [obtainAppLocaleNullable], but the [supplier] and this method's return type can't be null
+ */
+@JvmOverloads
+inline fun obtainAppLocale(supplier: () -> Locale = { Locale.getDefault() }) = obtainAppLocaleNullable(supplier)!!
+
+/**
+ * @return first app locale previously set by [AppCompatDelegate.setApplicationLocales] if present,
+ * else the locale returned by [supplier]
+ */
+inline fun obtainAppLocaleNullable(supplier: () -> Locale?) = AppCompatDelegate.getApplicationLocales()[0] ?: supplier()
