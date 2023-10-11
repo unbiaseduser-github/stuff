d232567bfaf1bb294bc27f755a318de965263444
bruh <quangtrung02hn16@gmail.com>
2023-10-11 11:30:15 +0700

    Bump version

--- stuff/build.gradle.kts
+++ stuff/build.gradle.kts
@@ -49,7 +49,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.sixtyninefourtwenty"
                 artifactId = "stuff"
-                version = "1.0"
+                version = "1.1"
 
                 from(components["release"])
 
